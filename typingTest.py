cab84767e2f7af3684c12e609d620d6992d872fe
HaaaO <zhonghas@usc.edu>
2021-04-16 10:19:24 -0700

    updated the input speed calculation

--- typingTest.py
+++ typingTest.py
@@ -18,161 +18,8 @@ import time
 import functools
 import Environment
 import Agent
+import sys
 # %matplotlib inline
-
-# class GridWorld:
-#     ## Initialise starting data
-#     def __init__(self):
-#         # Set information about the gridworld
-#         self.height = 1
-#         self.width = 5
-#         self.grid = np.zeros((self.height, self.width))
-                
-#         # Set random start location for the agent
-#         self.current_location = (0,  np.random.randint(0,5))
-        
-#         # Set locations for the bomb and the gold
-#         self.bomb_location = (0,0)
-#         self.gold_location = (0,4)
-#         self.terminal_states = [ self.bomb_location, self.gold_location]
-        
-#         # Set grid rewards for special cells
-#         self.grid[ self.bomb_location[0], self.bomb_location[1]] = -10
-#         self.grid[ self.gold_location[0], self.gold_location[1]] = 10
-#         self.grid[0,1] = -8
-#         self.grid[0,2] = -5
-#         self.grid[0,3] = 5
-
-#         # Set available actions
-#         self.actions = ['UP', 'DOWN', 'LEFT', 'RIGHT']
-    
-        
-#     ## Put methods here:
-#     def get_available_actions(self):
-#         """Returns possible actions"""
-#         return self.actions
-    
-#     def agent_on_map(self):
-#         """Prints out current location of the agent on the grid (used for debugging)"""
-#         grid = np.zeros(( self.height, self.width))
-#         grid[ self.current_location[0], self.current_location[1]] = 1
-#         return grid
-    
-#     def get_reward(self, new_location):
-#         """Returns the reward for an input position"""
-#         return self.grid[ new_location[0], new_location[1]]
-        
-    
-#     def make_step(self, action):
-#         """Moves the agent in the specified direction. If agent is at a border, agent stays still
-#         but takes negative reward. Function returns the reward for the move."""
-#         # Store previous location
-#         last_location = self.current_location
-
-        
-
-#         a = 1
-#         if a == 1:
-#             if self.current_location[1] == 4:
-#                 self.current_location = ( self.current_location[0], self.current_location[1])
-#                 reward = self.get_reward(self.current_location)
-#             else:
-#                 self.current_location = ( self.current_location[0], self.current_location[1]+1)
-#                 reward = self.get_reward(self.current_location)
-#         elif a == 0:
-#             self.current_location = ( self.current_location[0], self.current_location[1])
-#             reward = self.get_reward(self.current_location)
-#         else:
-#             if self.current_location[1] == 0:
-#                 self.current_location = ( self.current_location[0], self.current_location[1])
-#                 reward = self.get_reward(self.current_location)
-#             else:
-#                 self.current_location = ( self.current_location[0], self.current_location[1]-1)
-#                 reward = self.get_reward(self.current_location)
-        
-# #         # UP
-# #         if action == 'UP':
-# #             # If agent is at the top, stay still, collect reward
-# #             if last_location[0] == 0:
-# #                 reward = self.get_reward(last_location)
-# #             else:
-# #                 self.current_location = ( self.current_location[0] - 1, self.current_location[1])
-# #                 reward = self.get_reward(self.current_location)
-        
-# #         # DOWN
-# #         elif action == 'DOWN':
-# #             # If agent is at bottom, stay still, collect reward
-# #             if last_location[0] == self.height - 1:
-# #                 reward = self.get_reward(last_location)
-# #             else:
-# #                 self.current_location = ( self.current_location[0] + 1, self.current_location[1])
-# #                 reward = self.get_reward(self.current_location)
-            
-# #         # LEFT
-# #         elif action == 'LEFT':
-# #             # If agent is at the left, stay still, collect reward
-# #             if last_location[1] == 0:
-# #                 reward = self.get_reward(last_location)
-# #             else:
-# #                 self.current_location = ( self.current_location[0], self.current_location[1] - 1)
-# #                 reward = self.get_reward(self.current_location)
-
-# #         # RIGHT
-# #         elif action == 'RIGHT':
-# #             # If agent is at the right, stay still, collect reward
-# #             if last_location[1] == self.width - 1:
-# #                 reward = self.get_reward(last_location)
-# #             else:
-# #                 self.current_location = ( self.current_location[0], self.current_location[1] + 1)
-# #                 reward = self.get_reward(self.current_location)
-                
-#         return reward
-    
-#     def check_state(self):
-#         """Check if the agent is in a terminal state (gold or bomb), if so return 'TERMINAL'"""
-#         if self.current_location in self.terminal_states:
-#             return 'TERMINAL'
-
-# class RandomAgent():        
-#     # Choose a random action
-#     def choose_action(self, available_actions):
-#         """Returns a random choice of the available actions"""
-#         return np.random.choice(available_actions)   
-
-
-# class Q_Agent():
-#     # Intialise
-#     def __init__(self, environment, epsilon=0.05, alpha=0.1, gamma=1):
-#         self.environment = environment
-#         self.q_table = dict() # Store all Q-values in dictionary of dictionaries 
-#         for x in range(environment.height): # Loop through all possible grid spaces, create sub-dictionary for each
-#             for y in range(environment.width):
-#                 self.q_table[(x,y)] = {'UP':0, 'DOWN':0, 'LEFT':0, 'RIGHT':0} # Populate sub-dictionary with zero values for possible moves
-
-#         self.epsilon = epsilon
-#         self.alpha = alpha
-#         self.gamma = gamma
-        
-#     def choose_action(self, available_actions):
-#         """Returns the optimal action from Q-Value table. If multiple optimal actions, chooses random choice.
-#         Will make an exploratory random action dependent on epsilon."""
-#         if np.random.uniform(0,1) < self.epsilon:
-#             action = available_actions[np.random.randint(0, len(available_actions))]
-#         else:
-#             q_values_of_state = self.q_table[self.environment.current_location]
-#             maxValue = max(q_values_of_state.values())
-#             action = np.random.choice([k for k, v in q_values_of_state.items() if v == maxValue])
-        
-#         return action
-    
-#     def learn(self, old_state, reward, new_state, action):
-#         """Updates the Q-value table using Q-learning"""
-#         print(new_state)
-#         q_values_of_state = self.q_table[new_state]
-#         max_q_value_in_new_state = max(q_values_of_state.values())
-#         current_q_value = self.q_table[old_state][action]
-        
-#         self.q_table[old_state][action] = (1 - self.alpha) * current_q_value + self.alpha * (reward + self.gamma * max_q_value_in_new_state)
 
 
 class Ui_MainWindow(object):
@@ -229,6 +76,7 @@ class Ui_MainWindow(object):
 		self.work = WorkThread() #Work Thread
 		self.startTime=None
 		self.inputNum=0 #All keyboard inputNum
+		self.inputNumLast = 0
 		self.wpm=None #Word per minute
 		self.ipm=None #Input per minute
 
@@ -260,9 +108,17 @@ class Ui_MainWindow(object):
 		text=self.textEdit.toPlainText()
 		temp=filter(str.isalpha, text)
 		new_text=''.join(list(temp))
+		print(new_text)
 
-		wpm=round((len(new_text)/5)/time_interval)
-		ipm=round(self.inputNum/time_interval)
+		if self.inputNumLast == 0:
+			wpm=round((len(new_text)/5)/5)
+			ipm= self.inputNum/5
+			self.inputNumLast = self.inputNum
+		else:
+			print('!!')
+			wpm=round((len(new_text)/5)/5)
+			ipm=(self.inputNum - self.inputNumLast)/5
+			self.inputNumLast = self.inputNum
 
 		# print(time_interval)
 		# print(self.inputNum)
@@ -299,13 +155,11 @@ class WorkThread(QThread):
 
 	def run(self):
 		while 1:
-			time.sleep(1)
+			time.sleep(5)
 			self.trigger.emit(str(1))
 	
 
-
 if __name__ == "__main__":
-	import sys
 	app = QtWidgets.QApplication(sys.argv)
 	MainWindow = QtWidgets.QMainWindow()
 	ui = Ui_MainWindow()
